3b662ed5f9e724c76cd2371043dcc32cefa1dbd4
LoadingL0n3ly <fedor.khaldin@gmail.com>
2022-11-07 06:33:47 -0800

    ok

--- src/config.mjs
+++ src/config.mjs
@@ -1,11 +1,11 @@
 export const SITE = {
-	name: 'loadingl0n3ly',
+	name: 'l0n3ly',
 
 	origin: 'https://l0n3ly.com',
 	basePathname: '/',
 
-	title: 'loadingl0n3ly',
-	description: 'loadingl0n3ly',
+	title: 'l0n3ly',
+	description: 'my little portfolio corner of the internet',
 
 	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
 	googleSiteVerificationId: '',
